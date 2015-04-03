fe5b247b21a3a67f9d959326f1e8370ab3e7701a
Jinhwan <calanchue@naver.com>
2015-04-03 13:48:13 +0900

    commit test2

--- app/src/main/java/kr/ac/kaist/jinhwan/fluentkeyboard/InputFieldView.java
+++ app/src/main/java/kr/ac/kaist/jinhwan/fluentkeyboard/InputFieldView.java
@@ -4,12 +4,11 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * Created by Jinhwan on 2015-04-03.
- */
+
 public class InputFieldView extends View{
 
     public InputFieldView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        //test change
     }
 }
