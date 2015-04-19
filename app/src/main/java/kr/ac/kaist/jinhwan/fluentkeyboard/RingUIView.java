bf8cfe11325294e6d48898fb4ee4b46949d0bec7
Jinhwan <calanchue@naver.com>
2015-04-19 18:34:48 +0900

    for synchronization

--- app/src/main/java/kr/ac/kaist/jinhwan/fluentkeyboard/RingUIView.java
+++ app/src/main/java/kr/ac/kaist/jinhwan/fluentkeyboard/RingUIView.java
@@ -8,18 +8,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import java.util.LinkedList;
+
 public class RingUIView extends RelativeLayout {
     private String[] _keySet = {"A", "B", "C", "D", "E", "F", "G"};
-    private double UI_SIZE = 200;
+    private double UI_SIZE = 100;
 
+    LinkedList<AlphabetView> ringLeafList = new LinkedList<>();
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        float centerX = getX() + getWidth() / 2;
+        double radian = 0;
+        /*float centerX = getX() + getWidth() / 2;
         float centerY = getY() + getHeight() / 2;
+*/
+        float centerX = getWidth()/2;
+        float centerY = getHeight()/2;
+        UI_SIZE = 0.8 * getWidth()/2;
 
-        Log.e("ASDF", String.format("%f, %f, %d, %d", centerX, centerY, getWidth(), getHeight()));
+        Log.e("ASDF", String.format("%f, %f, %d, %d", getX(), getY(), getWidth(), getHeight()));
+
+        for (int i = 0; i < _keySet.length; i++) {
+            AlphabetView alphabetView = ringLeafList.get(i);
+            float childX = (float)(centerX+UI_SIZE*Math.cos(radian) - alphabetView.getWidth());
+            float childY = (float)(centerY+UI_SIZE*Math.sin(radian) - alphabetView.getHeight());
+           // Log.d("debug",String.format("child's position %d : %f %f", i ,childX , childY)  );
+           /* float childX = centerX + 10 * i;
+            float childY = centerY + 10 * i;*/
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) alphabetView.getLayoutParams();
+            //params.addRule(CENTER_IN_PARENT);
+            //params.addRule(ALIGN_PARENT_LEFT);
+            params.leftMargin = (int) childX;
+            params.topMargin = (int) childY;
+            //alphabetView.setLayoutParams(params);
+            radian += Math.PI / 4;
+        }
+        //invalidate();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+
+        //Log.e("ASDF", String.format("%f, %f, %d, %d", centerX, centerY, getWidth(), getHeight()));
         double radian = 0;
         for (int i = 0; i < _keySet.length; i++) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -27,21 +60,8 @@ public class RingUIView extends RelativeLayout {
             AlphabetView alphabetView = (AlphabetView) view;
             alphabetView.setText(_keySet[i]);
             this.addView(alphabetView);
-            /*float childX = (float)(centerX+UI_SIZE*Math.cos(radian) - alphabetView.getWidth());
-            float childY = (float)(centerY+UI_SIZE*Math.sin(radian) - alphabetView.getHeight());*/
-            float childX = centerX + 20 * i;
-            float childY = centerY + 20 * i;
-            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
-            params.leftMargin = (int) childX;
-            params.topMargin = (int) childY;
-            radian += Math.PI / 4;
+            ringLeafList.add(alphabetView);
         }
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
 
 
     }
